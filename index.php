20232e0f96efa50de8a5fce91d8be20eba88128a
Jassen Japheth Gaddiun <eflex9jamedia@gmail.com>
2023-11-16 08:36:57 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<!-- Created By Jassen Japheth -->
+<html lang="en">
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4594807928142645"
+     crossorigin="anonymous"></script>
+     
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jassen Japheth G | Portfolio</title>
+	<link rel="icon" type="image/x-icon" href="images/favicon.ico">
+  <link rel="shortcut icon"images/developer.JPG?_t=1666631110" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><style type="text/css">
+<!--
+body {
+	background-color: #F0F0F0;
+}
+.style1 {color: crimson}
+-->
+</style></head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Jassen<span> Japheth G.</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#teams" class="menu-btn">Teams</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+				<li><a href="https://www.youtube.com/@jayjassentech" class="menu-btn">YOUTUBE</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, my name is</div>
+                <div class="text-2">Jassen Japheth G.</div>
+                <div class="text-3">And I'm a <span class="typing">Neural Network & Deep Learning Expert</span></div>
+                <a href="https://facebook.com/CEOeflex9ja">Hire Me Today</a>
+                <a href="App/MyApp.apk">Download My App</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/developer.JPG" alt="Jassen Japheth" border="5">
+                </div>
+              <div class="column right">
+                    <div class="text"><span class="style1">Jassen Japheth G</span>, i am a <span class="typing-2">Programmer</span></div>
+                    <p>A B.Sc. Holder Computer Science (Hon.) from the prestigious Taraba State University popularly known with the acronym 'TSU'. I am also a Diploma Holder in Computer Science (ND) from Taraba State Polytechnic Suntai, Jalingo Campus. After series of basic courses and one (1) year compulsory National Youth Service in Kano State, North Nigeria, i am currently a professional with certificates to back my proficiency in the following areas of computing; Neural Network and Deep Learning Specialization and lots more...</p>
+                  <a href="doc/Resume.docx">Download My Resume</a>                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Web Design</div>
+                        <p>I started Web Design in the year 2018. In line, I have designed lots of websites, some among which i offer managerial services to them.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Advertising</div>
+                        <p>I create, write and direct mind-blowing contents to promote businesses and individuals. I offer the best advert ever in both visual, written and audio formats.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Apps Design</div>
+                        <p>I Design user-friendly mobile application for both iOS and Andriod. Some among the application I designed includes; VTU Mobile App, Artist Music Collection App, Student Final Clearance App, Student Photo Album, School Activities App and lots more.</p>
+                    </div>
+                </div>
+          </div>
+      </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My creative skills & experiences.</div>
+                    <p>To me coding is me and I am coding. I believe if you can dream it, i can build it and still polish it to standard that will bring out the best visualization in reality. Programming is one aspect of my life that i can't stay a day without visiting.</p>
+                    <a href="https://wa.me/2347088850874">Read more</a></br>
+                    <a href="App/JayJassenTech.apk"><img src="images/android.png" width="130" height="50"/></a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>Web Design</span>
+                            <span>99%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Artificial Intelligence</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Social Media</span>
+                            <span>97%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>System And Mobile App</span>
+                            <span>64%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Machine Learning</span>
+                            <span>84%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">My Team</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="images/nasi.jpg" alt="Nasi Uwhezuka">
+                        <div class="text">Nasi Uwhezuka</div>
+                        <p>Nasi Uwhezuka has been part of my team since 2018 when i created my first promotion platform known as Zontel and later modified as EFEX9JA MEDIA.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/skod.jpg" alt="">
+                        <div class="text">Skod Prince</div>
+                        <p>Skod Prince joined the team in late 2019 as the team artist but with the growth of the company now, he is the Director Human Resource.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/yumzy.jpg" alt="">
+                        <div class="text">Yumzy Dollar</div>
+                        <p>Yumzy Dollar is an active user of the mobile and PC. He is set to discover powerful tools in other to enhence and better the 21st Century Technology and he is currently the Top-leading Media Aid of the Company since 2019.</p>
+                        <a href="https://wa.me/2348138864442">Contact Him</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/Kingz.jpg" alt="">
+                        <div class="text">MC Kings</div>
+                        <p>MC Kings joined the team in early 2021 as the team first comedian following the policy of the company, MC Kings is now the official team event planner and director.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/zeus.jpg" alt="">
+                        <div class="text">Lord Zeus</div>
+                        <p>Being an afro-pop singer, Lord Zeus joined the team in 2019 as the team Artist. Lord Zeus owns the company's heart musically and the brand ambassador to foriegn policies.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/Queen.jpg" alt="">
+                        <div class="text">Queen Jay Jerry</div>
+                        <p>Queen Jay Jerry is the onboard Face of the Team and steadily been winning trophy for the team via pageantry also.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+		  <div align="center"><img src="images/favicon.png" width="268" height="268"/>	      </div>
+		  <h2 class="title">Contact Me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>After going through this information above i guess you want to contact me right? Feel free to contact me via any of this method below;</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Jassen Japheth G.</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Contact</div>
+                                <div class="sub-title">+234(0)70-888-50-874 (Nigeria)</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">jayjassentech@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Message.." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+      </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Designed By <a href="https://www.youtube.com/channel/UCKbecguZkp9IDEstm4RlMsQ">Jassen Japheth G. </a> | <span class="far fa-copyright"></span> 2023 All rights reserved.</span>    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
