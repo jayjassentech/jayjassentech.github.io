d8739573f96ec936456efdbfa286a30edf95ec91
Jassen Japheth Gaddiun <eflex9jamedia@gmail.com>
2023-11-17 09:53:07 -0800

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = "your_email@example.com"; // Replace with your email address
+    $headers = "From: $name <$email>";
+
+    $body = "Subject: $subject\n\n";
+    $body .= "From: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Message:\n$message";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Failed to send email.";
+    }
+}
+?>
